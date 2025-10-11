137ce6d633feb5ea7ed1b1b29ad1553c9db27733
mitsuiakane <akane.sumitomo@gmail.com>
2025-10-11 14:30:44 +0900

    クイズ機能実装

--- judge/views.py
+++ judge/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import FoodItem
 import random
+
 def top_view(request):
     return render(request, "top.html")
 
-
+print('あ')
 # def input_view(request):
 #     if request.method == 'POST':
 #         food_name = request.POST.get('food_name')
@@ -13,37 +14,39 @@ def top_view(request):
 #         return render(request, 'judge/result.html', {'food_name': food_name, 'is_meat': is_meat})
 #     return render(request, 'judge/input.html')
 def input_view(request):
-    foods = {
-    "唐揚げ": False,
-    "牛丼": True,
-    "チキン南蛮": False,
-    "ハンバーグ": True,
-    "焼き鳥": False,
-    "豚カツ": True,
-}
+    like_foods = ["マヨネーズ", "マックのハンバーガー", "ソーセージ"]
+    dislike_foods = ["唐揚げ", "チキン南蛮", "焼き鳥","ハンバーグ", "牛丼", "豚カツ"]
+
+    all_foods = like_foods + dislike_foods
     if request.method == "POST":
         food_name = request.POST.get("food_name")
-        user_answer = request.POST.get("answer")
-        result = judge_watanabe(food_name, user_answer)
+        user_answer = request.POST.get("answer")  # "好き" or "嫌い"
+        
+        # 正解を取得
+        print(food_name)
+        if food_name in like_foods:
+            correct_answer = "好き"
+            
+        else:
+            correct_answer = "嫌い"
+        print(correct_answer)
+        # ユーザーが選んだ答えと正解を比較
+        correct = (user_answer == correct_answer)
+        print(user_answer)
+        print(correct)
         return render(request, "judge/result.html", {
             "food_name": food_name,
-            "correct": result
+            "correct": correct,
+            "correct_answer": correct_answer
         })
-    food = random.choice(list(foods.keys()))
+ 
+    # 出題する食べ物をランダムに選ぶ
+    food = random.choice(all_foods)
     return render(request, "judge/input.html", {"food": food})
-
 
 def judge_watanabe(food_name, user_answer):
     # 渡辺さんの食べ物データベース
-    foods = {
-        "唐揚げ": False,
-        "牛丼": True,
-        "チキン南蛮": False,
-        "ハンバーグ": True,
-        "焼き鳥": False,
-        "豚カツ": True,
-    }
-
+  
     # 正解データを取得
     correct_answer = foods.get(food_name)
 
