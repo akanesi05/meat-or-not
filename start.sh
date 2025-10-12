fa35ca1fefc8d7ef0729df76d95157ed9ab9650b
mitsuiakane <akane.sumitomo@gmail.com>
2025-10-12 12:25:18 +0900

    start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
+# python manage.py runserver 0.0.0.0:$PORT
+
+#!/bin/bash
+set -e
+
+# マイグレーション
 python manage.py migrate --noinput
+
+# 静的ファイル収集（CSSなど）
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:$PORT
+
+# 本番サーバー起動 (Gunicorn)
+gunicorn watanabe_meat_judge.wsgi:application --bind 0.0.0.0:$PORT
